02c72ce60d81ba27294fe9ad16005b558f367edc
Murali Krishna Puvvada <muralikrishna.puvvada@gmail.com>
2020-02-05 11:51:59 +0530

    Containerise using docker-compose

--- Kotlin_producer_v2-master/src/main/resources/data.sql
+++ Kotlin_producer_v2-master/src/main/resources/data.sql
@@ -1,13 +1,2 @@
-DROP TABLE IF EXISTS claims;
-
-CREATE TABLE claims (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  created_by VARCHAR(250) NOT NULL,
-  claim_id VARCHAR(250) NOT NULL
-
-);
-
-INSERT INTO claims (created_by, claim_id) VALUES
-  ('Ganesh Sahu', 'FYO90877'),
-  ('Bill', 'HJ8909234'),
-  ('Folrunsho', 'YT67534');
+INSERT IGNORE INTO roles(name) VALUES('ROLE_USER');
+INSERT IGNORE INTO roles(name) VALUES('ROLE_ADMIN');
